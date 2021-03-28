48a3b35904ba0aed2ccbca95a6d63d9bd1bb52f8
Vignesh Narasimhulu <vigindian@gmail.com>
2021-03-28 13:56:34 +0000

    apache waf conf update

--- apache2/apache_waf.sh
+++ apache2/apache_waf.sh
@@ -7,12 +7,12 @@ sudo cp ${MODSECPATH}/modsecurity.conf-recommended  ${MODSECPATH}/modsecurity.co
 
 sudo systemctl reload apache2
 
-CONFS="default_waf wordpress_bestideas_waf"
+CONFS="default_waf"
 
 for CONF in $CONFS
 do
 	APACHECONF=$(echo $CONF | sed "s/_waf//g")
-	sudo cp conf/${CONF}.conf /etc/apache2/sites-available/${APACHECONF}
+	sudo cp conf/${CONF}.conf /etc/apache2/sites-available/${APACHECONF}.conf
 	sudo a2ensite ${APACHECONF}
 	sudo systemctl reload apache2
 done
